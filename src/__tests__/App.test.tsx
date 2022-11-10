28d3b8a00b7539424581c836bae3c841f7511728
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2022-11-10 15:39:08 +0000

    minor fix

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,6 +1,5 @@
 import { render } from '@testing-library/react';
 import App from '../App';
-import Accordion from '../components/Accordion';
 
 describe('<App />', () => {
   it('should render without errors', () => {
@@ -10,7 +9,3 @@ describe('<App />', () => {
     
   });
 });
-
-
-// ATTENTION!
-// Couldn't finish this test suit as I ran out of available time. However, it needs a little tweak and all will be well. I've left it on just for reference.
